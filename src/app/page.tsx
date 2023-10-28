667ddb9a04d62b27df7054175983b291497d3782
Yalalt <yalaltgandush@gmail.com>
2023-10-28 23:25:41 +0800

    Data cookie fetching on server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,41 @@
-'use client';
-import { message } from 'antd';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { cookies } from 'next/headers';
 
-export default function Home() {
-const [user, setUser] = useState<any>(null);
-
-const getUser = async () => {
+// return user data
+// server side data fetching
+// This function compiled and run on server side
+export async function getUser() {
   try {
-    const response = await axios.get('/api/users/currentuser');
-    setUser(response.data.data);
-
-  } catch (error: any) {
-    message.error(error.response.data.message || error.message);
+    const token = cookies().get('__session-sharejobs');
+    const response = await axios.get(`http://localhost:3000/api/users/currentuser`, {
+      headers: {
+        Cookie: `__session-sharejobs=${token?.value}`
+      }
+    });
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
   }
 }
 
-useEffect(() => {
-  getUser();
-},[]);
+export default async function Home() {
+  const user: any = await getUser();
+  // client side data fetching
+// const [user, setUser] = useState<any>(null);
+
+// const getUser = async () => {
+//   try {
+//     const response = await axios.get('/api/users/currentuser');
+//     setUser(response.data.data);
+
+//   } catch (error: any) {
+//     message.error(error.response.data.message || error.message);
+//   }
+// }
+
+// useEffect(() => {
+//   getUser();
+// },[]);
 
   return (
     <div>
